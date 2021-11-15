5d309ea31635804a5ed59b6c6e35f50869b290b1
seomaks <m.voevodin88@gmail.com>
2021-11-15 09:08:45 +0300

    first commit

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import style from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={style.header}>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/nav/Nav.js
@@ -0,0 +1,10 @@
+import style from './Nav.module.css';
+
+function Nav() {
+  return (
+    <div className={style.nav}>
+    </div>
+  );
+}
+
+export default Nav;
