7168e2bdddf48ca6219ab0d8817dbfd16ecbf0a0
seomaks <m.voevodin88@gmail.com>
2023-05-24 23:10:21 +0300

    updated link of backlinks checker

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -35,7 +35,7 @@ function Projects() {
           <Project style={todoList} title={"ToDoList"} description={"Application for creating a to-do list. Used technologies: TypeScript, React, Redux, Redux Toolkit, Redux Thunk, Material-UI, Formik, Jest testing"} link={"https://seomaks.github.io/todolist-ver2/"}/>
           <Project style={socialNW} title={"Social Network"} description={"Social network application. Used technologies: TypeScript, React, Redux, Redux Thunk, Redux-form, Axios, Reselect"} link={"https://seomaks.github.io/social-network/"}/>
           <Project style={wordMeaning} title={"Words meaning"} description={"Application for learning English. Used technologies: TypeScript, React, Redux, Redux Thunk, Axios"} link={"https://seomaks.github.io/words-meaning/"}/>
-          <Project style={linksChecker} title={"Backlinks checker"} description={"Application aimed at SEO-specialists to check the presence of a link in an article and indexing status in Google.. Used technologies: TypeScript, React, Redux, Redux Thunk, Axios, Material UI, Jest, XLSX"} link={"https://links-checker-demo.herokuapp.com"}/>
+          <Project style={linksChecker} title={"Backlinks checker"} description={"Application aimed at SEO-specialists to check the presence of a link in an article and indexing status in Google.. Used technologies: TypeScript, React, Redux, Redux Thunk, Axios, Material UI, Jest, XLSX"} link={"https://seomaks.github.io/backlinks-checker/"}/>
         </div>
       </div>
       </Fade>
